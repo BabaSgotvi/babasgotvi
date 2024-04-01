6c176f80ab5d89e426210d4837e0f814717096ac
HogPro <109669833+HogPro@users.noreply.github.com>
2024-04-01 19:06:27 +0200

    commit

--- secretVars.js
+++ secretVars.js
@@ -1,1 +1,1 @@
-const Stripe_Secret_Key = 'something'; // DO NOT SHARE. IF OBTAINED, THE STRIPE ACCOUNT CAN BE EMPTIED
+const Stripe_Secret_Key = 'change'; // DO NOT SHARE. IF OBTAINED, THE STRIPE ACCOUNT CAN BE EMPTIED
