c45688b86dfa8c45ceaad319a60c62392af7beca
HogPro <109669833+HogPro@users.noreply.github.com>
2024-04-02 20:58:05 +0200

    Fuck yeah!, got the damn payments to work. Took me over 30 hours

--- src/pages/Checkout.gwk2m.js
+++ src/pages/Checkout.gwk2m.js
@@ -1,9 +1,18 @@
+// @ts-ignore
 import * as Pay from 'backend/Pay';
 import wixPayFrontend from 'wix-pay-frontend';
 import { local, session, memory } from "wix-storage-frontend";
+const STRIPE_PUBLISHABLE_KEY = 'pk_live_51OLgRxCW4moslVDKuOA45Tpc0cQicgRqM8kr3lrCMqtFkeIJAcGs1TJjaNvXj9UXeM1hvkAIqrMUh9Y0ZS4FHs8700gdx0mKmw';
+// @ts-ignore
+import { charge } from 'backend/stripeProxy';
+import { createToken, encodeCard } from "public/stripeAPI.js";
 let ids = [];
 let amounts = [];
 retrieveCart();
+$w.onReady(async function () {
+    console.log("script is running");
+});
+
 function retrieveCart() {
     ids = JSON.parse(session.getItem("CheckoutIds"));
     amounts = JSON.parse(session.getItem("CheckoutAmounts"));
@@ -17,10 +26,51 @@ $w("#payButton").onClick((event) => {
 
 $w("#button1").onClick(() => {
     console.log("clicked");
-    Pay.transferToBankAccount(150, 'bgn', 'BG45INTF40012090790288')
-        .then(transfer => console.log('Transfer successful:', transfer))
-        .catch(error => console.error('Transfer failed:', error));
+    payNow();
+    console.log("payNow has been executed");
+    // Pay.transferToBankAccount(150, 'bgn', 'BG45INTF40012090790288')
+    //     .then(transfer => console.log('Transfer successful:', transfer))
+    //     .catch(error => console.error('Transfer failed:', error));
 });
+
+
+export function payNow() {
+    let payment = {
+        "amount": ($w("#amount").value * 100),
+        "currency": "BGN",
+        "description": $w("#description").value
+    }
+    createToken(encodeCard(createCard()))
+        .then((token) => {
+            charge(token, payment)
+                .then((response) => {
+                    if (response.chargeId) {
+                        console.log("Charge ID: " + response.chargeId);
+                        // $w('#response').text = response.chargeId;
+                        // $w('#lblResponse').text = "Charge ID (response from Stripe):";
+                        // $w('#btnClearPaid').label = "CLEAR PAID";
+                        // $w('#btnClearPaid').show();
+                    }
+                    else {
+                        // $w('#response').text = response.error;
+                        // $w('#lblResponse').text = "Processing error (from Stripe):";
+                        console.log(response.error);
+                    }
+                    // $w('#response').show();
+                });
+        });
+}
+
+
+function createCard() {
+    return {
+        "name": $w("#cardholder").value,
+        "number": $w("#cardnum").value,
+        "cvc": $w("#cvc").value,
+        "exp_year": $w("#year").value,
+        "exp_month": $w("#month").value
+    };
+}
 
 //
 /// TODO: CHECKOUT
---
+++ src/public/stripeAPI.js
@@ -0,0 +1,27 @@
+import { fetch } from 'wix-fetch';
+const apiKey = "pk_live_51OLgRxCW4moslVDKuOA45Tpc0cQicgRqM8kr3lrCMqtFkeIJAcGs1TJjaNvXj9UXeM1hvkAIqrMUh9Y0ZS4FHs8700gdx0mKmw"; // (public key)
+export async function createToken(card) {
+    const response = await fetch("https://api.stripe.com/v1/tokens", {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer " + apiKey
+        },
+        body: card
+    });
+    if (response.status >= 200 && response.status < 300) {
+        const json = await response.json()
+        return json.id;
+    }
+    const responseText = await response.text();
+    return response.status;
+}
+
+// Builds the fetch body from the array of card details.
+export function encodeCard(card) {
+    let encoded = "";
+    for (let [k, v] of Object.entries(card)) {
+        encoded = encoded.concat("card[", k, "]=", encodeURI(v), "&");
+    }
+    return encoded.substr(0, encoded.length - 1);
+}
