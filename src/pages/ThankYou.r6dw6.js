f6d37fa5b7986f4837a7250a5c6dbab518099d1e
HogPro <109669833+HogPro@users.noreply.github.com>
2024-04-10 09:04:52 +0300

    Add createId function and update issueOrder and deleteOrder functions

--- src/pages/ThankYou.r6dw6.js
+++ src/pages/ThankYou.r6dw6.js
@@ -1,10 +1,33 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import { fetch } from 'wix-fetch'; // Import Wix Fetch library
+import * as Secrets from 'backend/secrets';
+// Function to send SMS using Twilio
+async function sendSMSNotification(phoneNumber, message) {
+    const accountSid = 'AC8118e6cba5ba315d316ee4de5007e0b0';
+    const authToken = Secrets.TWILIO_TEST_AUTH_TOKEN;
+    const twilioPhoneNumber = '+13344534393';
+
+    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+
+    const headers = {
+        'Authorization': 'Basic ' + btoa(`${accountSid}:${authToken}`),
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    const body = `To=${encodeURIComponent(phoneNumber)}&From=${encodeURIComponent(twilioPhoneNumber)}&Body=${encodeURIComponent(message)}`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: body
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+}
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    let userPhoneNumber = '+359896830657';
+    let notificationMessage = 'Hello, you have a new notification!';
 
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    sendSMSNotification(userPhoneNumber, notificationMessage);
 });
