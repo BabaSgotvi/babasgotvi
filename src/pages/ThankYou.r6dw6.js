8ed71eebc9ce2386355fe9cc3e0d287656882035
HogPro <109669833+HogPro@users.noreply.github.com>
2024-04-12 12:06:53 +0300

    Add SendGrid email functionality

--- src/pages/ThankYou.r6dw6.js
+++ src/pages/ThankYou.r6dw6.js
@@ -5,6 +5,5 @@ import * as SendNotification from 'backend/SendNotification';
 
 $w.onReady(async function () {
     console.log("script is running");
-    SendNotification.sendSMSNotificationFETCH("+359896830657", "Helo from Wix Fetch");
-    // SendNotification.sendSMSNotificationMODULE("+359896830657", "Helo from Twilio Module");
+    SendNotification.sendEmail("duckata@gmail.com", "Този имейл е за тестови цели", "Това е тестово съобщение.");
 });
